830c090a968732c623929cf138f9b836355d2c0e
daniloof <59210244+daniloof@users.noreply.github.com>
2026-04-13 20:33:25 +0000

    feat: create address table

--- src/adapters/outbound/db/models.py
+++ src/adapters/outbound/db/models.py
@@ -4,9 +4,25 @@ from sqlalchemy.orm import declarative_base
 import uuid
 from datetime import datetime
 from sqlalchemy.sql import func
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
+class AddressModel(Base):
+    __tablename__ = "adresses"
+    
+    id = Column(UUID(as_uuid=True),primary_key=True,default=uuid.uuid4)
+    customer_id = Column(UUID(as_uuid=True),ForeignKey("customers.id"),nullable=False)
+    street = Column(String,nullable=False)
+    city = Column(String,nullable=False)
+    state = Column(String,nullable=False)
+    zip_code = Column(String,nullable=False)
+    created_at = Column(DateTime(timezone=True),server_default=func.now())
+
+    customer = relationship("CustomerModel",back_populates="adresses")
+
+    
 class CustomerModel(Base):
     __tablename__ = "customers"
 
@@ -14,3 +30,5 @@ class CustomerModel(Base):
     name = Column(String,nullable=False)
     email = Column(String, nullable=False, unique=True)
     created_at = Column(DateTime(timezone=True),server_default=func.now())
+
+    adresses = relationship("AddressModel",back_populates="customer")
